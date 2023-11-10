9d0a20f4c040cf2b1c07e1af205015b275084d69
Elena Manshina <Lenika2000@yandex.ru>
2023-11-10 23:04:21 +0300

    all_tasks: Соединение всех версий в рамках одного приложения - добавление промежуточных целей, отправка задания и версии

--- src/localStorage/getAppVersion.js
+++ src/localStorage/getAppVersion.js
@@ -13,27 +13,39 @@ export const initYandexValues = (version) => {
   switch (version) {
     case 0:
       setVersionAndTask(1,1);
-      console.log('App version = 1: Без фильтров по категориям, состав пиццы отображается в корзине, поиск только по названию пицц');
+      window.ym(95153636,'reachGoal','version1')
+      window.ym(95153636,'reachGoal','task1')
+      console.log('App version = 1: Без фильтров по категориям, состав пиццы отображается в корзине, поиск по названию пицц и ингредиентам');
       break;
     case 1:
       setVersionAndTask(1,2);
-      console.log('App version = 1: Без фильтров по категориям, состав пиццы отображается в корзине, поиск только по названию пицц');
+      window.ym(95153636,'reachGoal','version1')
+      window.ym(95153636,'reachGoal','task2')
+      console.log('App version = 1: Без фильтров по категориям, состав пиццы отображается в корзине, поиск по названию пицц и ингредиентам');
       break;
     case 2:
       setVersionAndTask(1,3);
-      console.log('App version = 1: Без фильтров по категориям, состав пиццы отображается в корзине, поиск только по названию пицц');
+      window.ym(95153636,'reachGoal','version1')
+      window.ym(95153636,'reachGoal','task3')
+      console.log('App version = 1: Без фильтров по категориям, состав пиццы отображается в корзине, поиск по названию пицц и ингредиентам');
       break;
     case 3:
       setVersionAndTask(2,1);
-      console.log('App version = 2: Есть фильтры по категориям');
+      window.ym(95153636,'reachGoal','version2')
+      window.ym(95153636,'reachGoal','task1')
+      console.log('App version = 2: Есть фильтры по категориям, поиск по названию пицц и ингредиентам');
       break;
     case 4:
       setVersionAndTask(3, 2);
-      console.log('App version = 3: Состав пиццы отображается при нажатии на тултип');
+      window.ym(95153636,'reachGoal','version3')
+      window.ym(95153636,'reachGoal','task2')
+      console.log('App version = 3: Состав пиццы отображается при нажатии на тултип, поиск по названию пицц и ингредиентам');
       break;
     case 5:
       setVersionAndTask(4,3);
-      console.log('App version = 4: Нет поиска по ингредиентам');
+      window.ym(95153636,'reachGoal','version4')
+      window.ym(95153636,'reachGoal','task3')
+      console.log('App version = 4: Нет поиска по ингредиентам, Нет фильтрации по категориям, Состав пиццы НЕ отображается при нажатии на тултип, состав пиццы не отображается в корзине ');
       break;
     default:
       return;
